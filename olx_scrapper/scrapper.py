21ec534d0bf59db55c0f32270d40bd5a8a759c74
JonatanLessa <jls15@aluno.ifal.edu.br>
2022-04-05 20:27:37 -0300

    Update the xpath

--- olx_scrapper/scrapper.py
+++ olx_scrapper/scrapper.py
@@ -9,7 +9,7 @@ wait_element_load('ad-list')
 # TODO: pagination
 for i in range(55):
     try:
-        product_link_xpath = '//*[@id="ad-list"]/li[' + str(i + 1) + ']/a'
+        product_link_xpath = '//*[@id="ad-list"]/li[' + str(i + 1) + ']/div/a'
         product_link_element = find_element_by_xpath(xpath=product_link_xpath)
         product_url = product_link_element.get_attribute('href')
 
@@ -28,6 +28,7 @@ for i in range(55):
         )
 
         row_arr = product_text_element.text.replace('\n', '|').split('|')
+        print(row_arr)
 
         if 'Online' in row_arr:
             row_arr.remove('Online')
@@ -35,13 +36,16 @@ for i in range(55):
         row_arr = [el for el in row_arr if 'de R$' not in el]
 
         # TODO: Fix this unpack
-        title, price, when, at, where, *_ = row_arr
+        title, price, *_, when, where, at = row_arr
+        print('\n')
+        print(where)
         city, district = where.split(' - ')[0].split(', ')
-
+        
         insert_place(city=city, district=district)
+        
     except Exception as e:
-        # print(e)
-        pass
+        print(e)
+        # pass
 
 close_db_connection()
 driver.quit()
