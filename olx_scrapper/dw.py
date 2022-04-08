e96fd2d982179d1b3eafe2f6b8855feea263e818
JonatanLessa <jls15@aluno.ifal.edu.br>
2022-04-08 02:43:39 -0300

    Adds the inserts in the dm_cellular and dm_annunciante tables

--- olx_scrapper/dw.py
+++ olx_scrapper/dw.py
@@ -3,14 +3,28 @@ import mysql.connector
 database_connection = mysql.connector.connect(
     host='localhost',
     user='root',
-    password='root',
+    password='Jon38Gal33$',
     database='olx_database'
 )
 
 def insert_place(city, district, state='Alagoas'):
     cursor = database_connection.cursor()
-    sql = "INSERT IGNORE INTO DM_LOCAL (CIDADE, BAIRRO, ESTADO) VALUES (%s, %s, %s)"
+    sql = "INSERT INTO dm_local (CIDADE, BAIRRO, ESTADO) VALUES (%s, %s, %s)"
     cursor.execute(sql, (city, district, state))
+    database_connection.commit()
+    cursor.close()
+
+def insert_mobile(brand, title, state):
+    cursor = database_connection.cursor()
+    sql = "INSERT INTO dm_celular (MARCA, MODELO, ESTADO) VALUES (%s, %s, %s)"
+    cursor.execute(sql, (brand, title, state ))
+    database_connection.commit()
+    cursor.close()
+
+def insert_advertiser(name):
+    cursor = database_connection.cursor()
+    sql = "INSERT INTO dm_anunciante (NOME) VALUES (%s)"
+    cursor.execute(sql, (name, ))
     database_connection.commit()
     cursor.close()
 
