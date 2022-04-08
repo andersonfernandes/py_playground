42844765dff976f0c74c926fd4d9495500b865c4
JonatanLessa <jls15@aluno.ifal.edu.br>
2022-04-08 05:13:32 -0300

    Capture publication data

--- olx_scrapper/scrapper.py
+++ olx_scrapper/scrapper.py
@@ -1,5 +1,5 @@
 import time
-from dw import insert_place, insert_mobile, insert_advertiser, close_db_connection
+from dw import insert_place, insert_mobile, insert_advertiser, insert_date, close_db_connection
 from chrome_driver import get_driver, find_element_by_xpath, wait_element_load
 
 driver = get_driver()
@@ -18,12 +18,14 @@ for i in range(6):
         driver.execute_script('window.open("' + product_url + '");')
         driver.switch_to.window(driver.window_handles[-1])
         time.sleep(3)
+        
         brand = driver.find_element_by_xpath('//*[@id="content"]/div[2]/div/div[2]/div[1]/div[22]/div/div/div/div[2]/div[2]/div/a').get_attribute('text')        
         state = driver.find_element_by_xpath('//*[@id="content"]/div[2]/div/div[2]/div[1]/div[22]/div/div/div/div[2]/div[3]/div/a').get_attribute('text')
         advertiserName = driver.find_element_by_xpath('//*[@id="miniprofile"]/div/div/div/div[2]/div/span').text
-        #print(brand)
-        #print(state)
-        #print(advertiserName)        
+        publicationData = driver.find_element_by_xpath('//*[@id="content"]/div[2]/div/div[2]/div[1]/div[26]/div/div/div/span[1]').text
+        listStrDate = publicationData.split()
+        listMothDay = listStrDate[2].split('/')
+      
         driver.close()        
         driver.switch_to.window(driver.window_handles[0]) # Go back to the main window
 
@@ -49,6 +51,7 @@ for i in range(6):
         insert_place(city=city, district=district)
         insert_mobile(brand, title, state)
         insert_advertiser(advertiserName)
+        insert_date(hour=listStrDate[4], day=listMothDay[0], moth=listMothDay[1])
         
     except Exception as e:
         print(e)
